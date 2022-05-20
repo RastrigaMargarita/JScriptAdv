700731d8b4b72292f1b73e86be61837baaca6a22
HP <rastrm@mail.ru>
2022-05-20 17:52:51 +0300

    styles

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const goods = [
     { title: 'Shirt', price: 150 },
     { title: 'Socks', price: 50 },
@@ -5,13 +7,13 @@ const goods = [
     { title: 'Shoes', price: 250 },
 ];
 
-const renderGoodsItem = (title, price) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+const renderGoodsItem = (title = "скоро тут будет товар", price = "0 EUR") => {
+    return `<div class="main__goods-item"><h3>${title}</h3><p>${price}</p></div>`;
 };
 
 const renderGoodsList = (list) => {
     let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList;
+    document.querySelector(".main__goods-list").innerHTML = goodsList.join(" ");
 }
 
 renderGoodsList(goods);
